efc78c60df33af950d9e383805fdd51ae89bc14e
Hoa Nguyen <kevinhoa95@gmail.com>
2016-06-21 10:02:03 +0700

    get data from json file to display

--- app/view/view-house/view-house.component.js
+++ app/view/view-house/view-house.component.js
@@ -2,15 +2,15 @@ angular.module('viewHouse')
 .component('viewHouse', {
 	templateUrl: 'view/view-house/view-house.template.html',
 	controller:['$http', function($http){
-		var url = 'http://localhost:3000/api/house/24?raw=1';
+		var url = '../../data/houses.json';
 		$http.get(url).then(function success(response){
 			console.log(response);
 			var self = this;
-			self.house = response.data.house;
+			self.house = response.data[0];
 			console.log(self.house);
 		},
 		function error(response){
 			// $scope.error = response.status;
 		});
 	}]
-});
+});	
