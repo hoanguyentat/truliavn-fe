b6112336e5b355da22659947977694976e00999d
Hola Nguyen <kevinhoa95@gmail.com>
2016-06-27 01:40:34 +0700

    add some route

--- app/app.config.js
+++ app/app.config.js
@@ -7,6 +7,32 @@ angular.
 	.when('/', {
 		template: '<index></index>'
 	})
+	.when('/houses', {
+		template: '<house-list></house-list>'
+	})
+	.when('/houses/:houseId', {
+		template: '<house-detail></house-detail>'
+	})
+	.when('/for-rent', {
+		template: '<house-list></house-list>'
+	})
+	.when('/for-sale', {
+		template: '<house-list></house-list>'
+	})
+	// house feature
+	.when('/houses', {
+		template: '<house-list></house-list>'
+	})
+	.when('/addhouse', {
+		template: '<house-list></house-list>'
+	})
+	.when('/:postId/delete', {
+		template: '<house-list></house-list>'
+	})
+	.when('/:postId/edit', {
+		template: '<house-list></house-list>'
+	})
+	// user feature
 	.when('/login', {
 		templateUrl: 'view/user/login/login.html',
 		controller: 'LoginController'
@@ -18,11 +44,9 @@ angular.
 	.when('logout', {
 		controller: 'LogoutController'
 	})
-	.when('/houses', {
+	.when('/:user/edit', {
 		template: '<house-list></house-list>'
 	})
-	.when('/houses/:houseId', {
-		template: '<house-detail></house-detail>'
-	})
+	
 	.otherwise('/');
 }]);
