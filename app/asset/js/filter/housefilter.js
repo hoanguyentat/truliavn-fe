c24ee276852fdc933e61ba7ea6fdfed7c48b35c1
Hola Nguyen <kevinhoa95@gmail.com>
2016-07-15 16:16:16 +0700

    fix upload file for house infomation

--- app/asset/js/filter/housefilter.js
+++ app/asset/js/filter/housefilter.js
@@ -22,6 +22,21 @@ app.filter('customDistrict', function() {
     return result;
   }
 });
+app.filter('customCity', function() {
+  return function(input, search) {
+    // console.dir(input);
+    if (!input) return input;
+    if (!search) return input;
+    var result = {};
+    angular.forEach(input, function(value, key) {
+      if (input[key].cityId == search) {
+        result[key] = input[key];
+      }
+    });
+    // console.log(result);
+    return result;
+  }
+});
 
 //filter houses by district, wards
 app.filter('houseDistrict', function() {
